54a60c8fd6188d67350bb883b0baeac4be70f761
Andrew-Codechimp <andrew@codechimp.org>
2014-07-08 16:23:00 +0100

    Restructured project ready for wear

---
+++ Wearable/src/main/java/org/codechimp/androidfortunes/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.android.wearable.delayedconfirmation;
+
+import android.app.Activity;
+import android.app.Notification;
+import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
+import android.support.wearable.view.DelayedConfirmationView;
+import android.util.Log;
+import android.view.View;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.wearable.MessageApi;
+import com.google.android.gms.wearable.Node;
+import com.google.android.gms.wearable.NodeApi;
+import com.google.android.gms.wearable.Wearable;
+
+
+public class MainActivity extends Activity {
+
+}
